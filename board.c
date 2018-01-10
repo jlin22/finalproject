eb638482b32b6a448209f1d6980ab8fb5b275539
Michael Lee <michaelleestuy@gmail.com>
2018-01-10 11:38:12 -0500

    added initial chess stuff

---
+++ board.c
@@ -0,0 +1,21 @@
+#include "chess.h"
+
+
+
+char*** startgame(){
+  char*** board = malloc(sizeof(char**));
+  return board;
+
+
+}
+
+
+void makemove(char* move, char*** board){
+  
+  
+
+}
+
+void main(){
+
+}
