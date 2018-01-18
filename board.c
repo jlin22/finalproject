8ca6ab22a8a3c04a106d8a6080110ddca47ddc3a
Michael Lee <michaelleestuy@gmail.com>
2018-01-18 11:47:24 -0500

    wip

--- board.c
+++ board.c
@@ -1,19 +1,28 @@
 #include "chess.h"
+#include <stdio.h>
+#include <string.h>
+
+struct board {char a[8][8];};
 
 
-
-char*** startgame(){
-  char*** board = malloc(sizeof(char**));
-  return board;
+struct board* startgame(){
+  struct board b = malloc(sizeof(struct board));
+  struct board* c = &b;
+  for(int i = 0; i < 8; i++){
+    for(int j = 0; j < 8; j++){
+      *c.a[i][j] = 'B';
+    }
+  }
+  
+  return c;
 
 
 }
 
 void printboard(char***board){
-  char** board2 = *board;
-  for(int i = 0; i < 8; i++){
+   for(int i = 0; i < 8; i++){
     for(int j = 0; j < 8; j++){
-      printf("%c ", board2[i][j]);
+      printf("%c ", *board[i][j]);
     }
     printf("\n");
   }  
@@ -21,11 +30,12 @@ void printboard(char***board){
 
 
 void makemove(char* move, char*** board){
-  
-  
+
 
 }
 
 void main(){
-
+  char*** board = startgame();
+  //printboard(board);
+  return;
 }
