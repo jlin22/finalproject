1cfec869d0cf23455abaa6148b4bcb22b3543997
saltonbench <jlin22@stuy.edu>
2018-01-23 12:23:09 -0500

    worked on loading game states

--- board.c
+++ board.c
@@ -1,6 +1,6 @@
 #include "chess.h"
-#include <stdio.h>
-#include <string.h>
+
+
 
 struct board {char a[8][8];};
 
@@ -773,22 +773,51 @@ void printboard(struct board* board){
 }
 
 
+
 void main(){
-  struct board* board = startgame();
-  printboard(board);
-  while(1){
-
-    char line[4];
-    fgets(line, 10, stdin);
-    int i = line[0] - '0';
-    int j = line[1] - '0';
-    int m = line[2] - '0';
-    int n = line[3] - '0';
-
-    movepieceWrap(board, i, j, m, n);
+  printf("insert command:");
+  char command[256];
+  fgets(command, sizeof(command), stdin);
+  command[strlen(command)-1] = 0;
+  if(strcmp(command, "ai")==0){
+    char name[256];
+    printf("choose file to write into:");
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name) -1] = 0;
+    printf("initializing game\n");
+    struct board* board = startgame();
     printboard(board);
+    while(1){
+      
+      char line[4];
+      fgets(line, 10, stdin);
+      int i = line[0] - '0';
+      int j = line[1] - '0';
+      int m = line[2] - '0';
+      int n = line[3] - '0';
+      
+      movepieceWrap(board, i, j, m, n);
+      printboard(board);
+      //write game states
+      remove(name);
+      int file = open(name, O_CREAT | O_WRONLY, 0600);
+      write(file, board,4*8*8);
       
    
+    }
+  }
+  else if (strcmp(command, "load")==0){
+    printf("file to load:");
+    char name[256];
+    fgets(name, sizeof(name), stdin);
+    name[strlen(name)-1]=0;
+    int file = open(name, O_RDWR, 0600);
+    struct board* board;
+    read(file, board,4*8*8);
+    //printboard(board);
+  }
+  else{
+    printf("invalid command");
   }
   return;
 }
--- chess.h
+++ chess.h
@@ -2,11 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-void interpret(char *);
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/*void interpret(char *);
 void menu(char *);
 void ai();
 void multiplayer();
 void load();
 void init_board(char[8][8]);
 void print_board(char[8][8]);
-void save();
+void save();*/
